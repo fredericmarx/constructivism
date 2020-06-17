53853bc3414f2cf6ba00d3e16716e4cfd4789ac2
Frederic Marx <marx.frederic@gmail.com>
2020-06-17 19:03:54 +0200

    Proof of concept: Scroll animation

--- src/_includes/partials/main.js
+++ src/_includes/partials/main.js
@@ -1,7 +1,25 @@
 import { defaultRectlist, defaultPreset, renderSvg } from "/js/main.js";
 
 const canvas = document.getElementById("canvas");
-const canvasHeight = 170;
-const canvasWidth = 240;
 
 canvas.src = renderSvg(defaultRectlist, defaultPreset);
+
+function adjustIndex(preset, index) {
+  const newPreset = Object.assign({}, preset, { index });
+  return newPreset;
+}
+
+function getScrollRatio() {
+  const h = document.documentElement;
+  const b = document.body;
+  const st = "scrollTop";
+  const sh = "scrollHeight";
+
+  const percent = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight);
+  return percent;
+}
+
+window.addEventListener("scroll", () => {
+  const preset = adjustIndex(defaultPreset, getScrollRatio() * 20);
+  canvas.src = renderSvg(defaultRectlist, preset);
+});
