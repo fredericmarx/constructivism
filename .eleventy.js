41d67e022dc3be14797fda1a665666af8ad13dd2
Frederic Marx <marx.frederic@gmail.com>
2020-08-14 13:29:21 +0200

    Clean up build

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/fonts");
-  eleventyConfig.addPassthroughCopy("src/js");
 
   return {
     dir: {
