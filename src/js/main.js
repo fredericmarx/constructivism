82451ce6d690039230e7dfbd1c3f77031ad3c845
Frederic Marx <marx.frederic@gmail.com>
2020-10-03 13:13:37 +0200

    Remove unneeded JS code

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 import { getRandomRectlist, defaultPreset, renderSvg } from "./lib/lib.mjs";
 
-const main = document.querySelector("main");
 const maxIndex = 50;
-
-let start;
 
 class Construction {
   constructor() {
@@ -226,65 +223,3 @@ class Construction {
 }
 
 const construction = new Construction();
-
-function getScrollRatio() {
-  const h = document.documentElement;
-  const b = document.body;
-  const st = "scrollTop";
-  const sh = "scrollHeight";
-
-  const percent = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight);
-  return percent;
-}
-
-function getElementDistance(el1, el2) {
-  const rect1 = el1.getBoundingClientRect();
-  const rect2 = el2.getBoundingClientRect();
-  const topDistance = rect2.top - rect1.bottom;
-  const bottomDistance = rect1.top - rect2.bottom;
-  return Math.max(topDistance, bottomDistance);
-}
-
-function clamp(number, lower, upper) {
-  if (number === number) {
-    if (upper !== undefined) {
-      number = number <= upper ? number : upper;
-    }
-    if (lower !== undefined) {
-      number = number >= lower ? number : lower;
-    }
-  }
-  return number;
-}
-
-function tick(timestamp) {
-  if (start === undefined) {
-    start = timestamp;
-  }
-
-  const distance = getElementDistance(canvas, main);
-  const mainHeight = main.clientHeight;
-  const ratio = Math.sqrt(mainHeight / 2 + distance) || 0;
-
-  const elapsed = timestamp - start;
-  const maxCount = clamp(ratio, 4, 12);
-  const rotate45Amount = 4 + Math.ceil(2 * Math.sin(elapsed / 1500));
-  const rotate90Amount = 6 + Math.ceil(4 * Math.cos(elapsed / 1500));
-  const rotate315Amount = 2 + Math.ceil(2 * Math.sin(elapsed / 1500));
-  const colorAmount = Math.pow(Math.sin(elapsed / 7000), 2) / 2;
-  const index = getScrollRatio() * maxIndex;
-
-  const preset = defaultPreset;
-  const newPreset = Object.assign({}, preset, construction.preset, {
-    //index,
-    //maxCount,
-    //rotate45Amount,
-    //rotate90Amount,
-    //rotate315Amount,
-    //colorAmount,
-  });
-
-  window.requestAnimationFrame(tick);
-}
-
-//window.requestAnimationFrame(tick);
